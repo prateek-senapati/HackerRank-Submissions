cf80e49f4289cec0b4eceb7f037a47bf4f7d1f89
Prateek Senapati <prateek1210s@gmail.com>
2019-06-29 21:39:31 +0530

    Add submissions to first two problems

---
+++ C++/cpp-hello-world.cpp
@@ -0,0 +1,11 @@
+// Say "Hello, World!" With C++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ C++/cpp-input-and-output.cpp
@@ -0,0 +1,13 @@
+// Input and Output
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
